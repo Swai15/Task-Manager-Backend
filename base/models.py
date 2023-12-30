4ebd3ce0556fb515f48bdd41249d253c918ba29b
Swai15 <swaisilvano5@gmail.com>
2023-12-30 15:37:07 +0300

    serializers base models

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from users.models import CustomUser
 
 class Project(models.Model):
   title = models.CharField(max_length=50)
+  owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
 
   def __str__ (self):
     return self.title
--- base/serializers.py
+++ base/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
 from .models import Task
 
+class ProjectSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Task
+    fields = '__all__'
+
+  def create(self, validated_data):
+    user = self.context['request'].user
+    validated_data['owner'] = user
+    return super().create(validated_data)
+
+
 class TaskSerializer(serializers.ModelSerializer):
   class Meta:
     model = Task
     fields = '__all__'
+
+  def create(self, validated_data):
+    user = self.context['request'].user
+    validated_data['owner'] = user
+    return super().create(validated_data)
+
+
