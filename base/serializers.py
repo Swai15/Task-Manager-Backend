bb05410d323a0f61ef0b045c7059ebcd8d45cde7
Swai15 <swaisilvano5@gmail.com>
2024-01-22 23:26:09 +0300

    unused validate funtions

--- base/serializers.py
+++ base/serializers.py
@@ -19,13 +19,7 @@ class TaskSerializer(serializers.ModelSerializer):
       default_user = CustomUser.objects.get(username='admin')
       validated_data['owner'] = default_user
       print(f"test") 
-
-    # project_title = validated_data.get('project', None)
-
-    # if project_title: 
-    #   project = Project.objects.get(title=project_title)
-    #   validated_data['project'] = project.id
-
+      
     return super().create(validated_data)
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -35,15 +29,6 @@ class ProjectSerializer(serializers.ModelSerializer):
     model = Project
     fields = ('id', 'title', 'tasks', 'icon')
     read_only_fields = ['id']
-
-  # def validate(self, validated_data):
-  #   request = self.context.validated_data('request')
-  #   if request and request.method == "PUT":
-  #     title = validated_data.get('title')
-  #     if not title:
-  #       raise serializers.ValidationError({'title': 'Title is required for updates'})
-  #   return validated_data
-  
 
   def create(self, validated_data):
     user = self.context['request'].user
